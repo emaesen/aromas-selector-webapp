acc74ead0e1290ca923c1eccfe33d484d944cac3
Edward <edward.maesen@gmail.com>
2021-04-30 12:47:11 -0700

    ensure data name uniqueness

--- server/src/models/aromas.model.js
+++ server/src/models/aromas.model.js
@@ -5,8 +5,20 @@ module.exports = function (app) {
   const dbPath = app.get('nedb');
   const Model = new NeDB({
     filename: path.join(dbPath, 'aromas.db'),
+    timestampData: true,
     autoload: true
+  });
+
+
+  // eslint-disable-next-line no-unused-vars
+  Model.ensureIndex({ fieldName: 'name', unique: true }, function(err) {
+    // handle errors...
+    // err.errorType, err.key, err.message
+    if (err) {
+      console.log("aromas.model ensureIndex error", err);
+    }
   });
 
   return Model;
 };
+
