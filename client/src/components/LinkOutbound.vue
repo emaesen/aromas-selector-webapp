b9c4e24598f7f3f64f9d3bbbc9480f458a578ea2
Edward <edward.maesen@gmail.com>
2021-06-02 15:25:24 -0700

    add LinkOutbound component

---
+++ client/src/components/LinkOutbound.vue
@@ -0,0 +1,27 @@
+<template>
+  <a class="link-outbound" :href="to" target="_blank" rel="noopener noreferrer"><slot></slot></a>
+</template>
+
+<script>
+
+export default {
+  name: 'LinkOutbound',
+  props: {
+    to: {
+      type: String,
+    },
+  },
+  components: {
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.link-outbound::after {
+  content: '⧉';
+  transform: scale(-1,1);
+  opacity: .6;
+  display: inline-block;
+  font-style: normal;
+}
+</style>
